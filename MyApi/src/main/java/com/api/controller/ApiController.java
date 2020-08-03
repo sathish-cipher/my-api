02b4e84e9ddd72e7d3e3787a8f06c9cac092cac3
Sathish R <sathishjj8@gmail.com>
2020-08-03 21:27:22 +0530

    first api

---
+++ MyApi/src/main/java/com/api/controller/ApiController.java
@@ -0,0 +1,13 @@
+package com.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+	
+	@PostMapping
+	public void addUser() {
+		
+	}
+}
